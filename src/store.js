ff371eb6fc9f58b0bf8f06ce0da2d5b9c7cc4b3d
AliceCaselli <alicecaselli3@gmail.com>
2023-04-02 21:04:28 +0200

    Milestone 0

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    APICall: 'https://api.themoviedb.org/3',
+
+    APIQuery: '',
+
+    APIKey: 'bd9455793911789f2178857713cc98a7',
+
+    SearchWord: '',
+});
